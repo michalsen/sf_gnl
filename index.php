e1898cfb20066701f9727437640f6fc268b69875
Eric Michalsen <eric.michalsen@gmail.com>
2017-10-12 11:03:18 -0500

    ci test

--- index.php
+++ index.php
@@ -7,12 +7,8 @@
 
 session_start();
 
-$api = [];
-
 require 'vendor/autoload.php';
-//require_once '.config_dev';
-
-
+require_once 'config.php';
 
 use \Siler\Functional as F;
 use \Siler\Route;
@@ -21,23 +17,12 @@ use \Phpforce\SoapClient\Client;
 use \GuzzleHttp\Exception\RequestException;
 use \GuzzleHttp\Psr7\Request;
 
-
 $page = (isset($_SESSION['valid']) ? 'login.twig' : 'home.twig');
 
-/**
- *  Postback call
- */
-// if (preg_match('/postback/', $_SERVER['REQUEST_URI'])) {
-//   if (isset($_POST['lead_id'])) {
-//     $lead = $_POST['lead_id'];
-//   }
-// }
-
-// include 'src/class.gnl.php';
+include 'src/class.gnl.php';
 // include 'src/class.sf.php';
 
 if (!isset($lead)) $lead = ['test 1', 'test 2'];
-
 
 Twig\init('templates');
 
@@ -49,6 +34,25 @@ Route\get('/', F\puts(Twig\render($page,
                   )
                 )
               );
+
+
+/**
+ *  Postback call
+ */
+// if (preg_match('/postback/', $_SERVER['REQUEST_URI'])) {
+//   if (isset($_POST['lead_id'])) {
+//     $lead = $_POST['lead_id'];
+//   }
+// }
+
+
+
+
+
+
+
+
+
 
 
 
